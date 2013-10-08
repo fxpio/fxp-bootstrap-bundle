172ced994437e62d27bdc2314e54f3f2871d1178
François Pluchino <francois.pluchino@sonatra.com>
2013-10-08 15:02:30 +0200

    Adds url form extension

---
+++ Form/Extension/UrlExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
+
+/**
+ * Url Form Extension.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class UrlExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $prependNormalizers = function (Options $options, $value) {
+            if (null === $value) {
+                $value = $options['default_protocol'] . '://';
+            }
+
+            return $value;
+        };
+
+        $resolver->setNormalizers(array(
+            'prepend' => $prependNormalizers,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'url';
+    }
+}
