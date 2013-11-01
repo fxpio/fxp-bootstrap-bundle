db8b1005bb950c3fdbf2a57445add05691ea56ee
François Pluchino <francois.pluchino@sonatra.com>
2013-11-01 18:19:42 +0100

    Adds body coby block

---
+++ Block/Type/ParagraphType.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Paragraph Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ParagraphType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'lead' => $options['lead'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'lead'  => false,
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'lead'  => 'bool',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'paragraph';
+    }
+}
