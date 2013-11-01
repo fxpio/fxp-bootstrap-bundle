f440332715b202321c3a86135cff16351da1b813
François Pluchino <francois.pluchino@sonatra.com>
2013-11-01 18:39:40 +0100

    Adds the typography abbreviations

---
+++ Block/Type/AbbreviationType.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Abbreviation Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class AbbreviationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'initialism' => $options['initialism'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'initialism' => false,
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'initialism' => 'bool',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'abbreviation';
+    }
+}
