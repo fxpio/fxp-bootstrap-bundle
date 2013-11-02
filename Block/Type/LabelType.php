e3fab595d1134f1fe7917ab48edb8cd3b794d9e2
François Pluchino <francois.pluchino@sonatra.com>
2013-11-02 11:18:30 +0100

    Adds label block

---
+++ Block/Type/LabelType.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Label Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class LabelType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'style' => $options['style'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'style' => 'default',
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'style' => 'string',
+        ));
+
+        $resolver->setAllowedValues(array(
+            'style' => array('default', 'primary', 'success', 'info', 'warning', 'danger', 'link'),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'label';
+    }
+}
