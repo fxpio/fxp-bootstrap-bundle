6f36755ac4c6d8e06700fdda8504a01800242fe7
François Pluchino <francois.pluchino@sonatra.com>
2013-11-02 11:36:37 +0100

    Adds badge block

---
+++ Block/Type/BadgeType.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Badge Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class BadgeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'pull_right' => $options['pull_right'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'pull_right' => false,
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'pull_right' => 'bool',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'badge';
+    }
+}
