84671c8578de81d39b325184f10efef54c39bc71
François Pluchino <francois.pluchino@sonatra.com>
2013-11-06 19:34:22 +0100

    Adds media object blocks

---
+++ Block/Type/MediaBodyType.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+
+/**
+ * Media Body Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class MediaBodyType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(BlockView $view, BlockInterface $block, array $options)
+    {
+        foreach ($view->children as $name => $child) {
+            if (in_array('heading', $child->vars['block_prefixes'])) {
+                $class = isset($child->vars['attr']['class']) ? $child->vars['attr']['class'] : '';
+
+                $class .= ' media-heading';
+                $class = trim($class);
+
+                $child->vars['attr']['class'] = $class;
+                $child->vars['size'] = 4;
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'media_body';
+    }
+}
---
+++ Block/Type/MediaType.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Media Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class MediaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'media_list' => false,
+            'tag'        => 'div',
+        ));
+
+        if (isset($view->parent->vars['media_list']) && $view->parent->vars['media_list']) {
+            $view->vars = array_replace($view->vars, array(
+                'media_list' => true,
+                'tag'        => 'li',
+            ));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(BlockView $view, BlockInterface $block, array $options)
+    {
+        foreach ($view->children as $name => $child) {
+            if (in_array('link', $child->vars['block_prefixes']) || in_array('image', $child->vars['block_prefixes'])) {
+                $class = isset($child->vars['attr']['class']) ? $child->vars['attr']['class'] : '';
+
+                if (in_array('image', $child->vars['block_prefixes'])) {
+                    $class .= ' media-object';
+                }
+
+                $class .= ' pull-' . $options['pull'];
+                $class = trim($class);
+
+                $child->vars['attr']['class'] = $class;
+
+                foreach ($child->children as $subName => $subChild) {
+                    if (in_array('image', $subChild->vars['block_prefixes'])) {
+                        $subClass = isset($subChild->vars['attr']['class']) ? $subChild->vars['attr']['class'] : '';
+
+                        $subClass .= ' media-object';
+                        $subClass = trim($subClass);
+
+                        $subChild->vars['attr']['class'] = $subClass;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'pull' => 'left',
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'pull' => 'string',
+        ));
+
+        $resolver->setAllowedValues(array(
+            'pull' => array('left', 'right'),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'media';
+    }
+}
