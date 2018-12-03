89f095185dc74e1a508b43f478105e693be24869
François Pluchino <francois.pluchino@gmail.com>
2018-12-03 11:15:30 +0100

    Update to Symfony 4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,6 +11,7 @@
 
 namespace Fxp\Bundle\BootstrapBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -26,8 +27,9 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fxp_bootstrap');
+        $treeBuilder = new TreeBuilder('fxp_bootstrap');
+        /* @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
